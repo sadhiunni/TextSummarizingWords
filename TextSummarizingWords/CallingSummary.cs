27889918868855aa7206ce2343b4e37bb2ea1377
sadhiunni <76890340+sadhiunni@users.noreply.github.com>
2021-02-22 19:23:50 +0530

    Add project files.

---
+++ TextSummarizingWords/CallingSummary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextSummarizingWords
+{
+        public class CallingSummary
+        {
+            public static string WritingSummary(string Text, int maxLength)
+            {
+                int totalCharacter = 0;
+                List<string> summary = new List<string>();
+                if (Text.Length < maxLength)
+                {
+                    Console.WriteLine(Text);
+                }
+                else
+                {
+                    var words = Text.Split(" ");
+                    foreach (var word in words)
+                    {
+                        totalCharacter += word.Length;
+                        if (totalCharacter >= maxLength)
+                            break;
+                        summary.Add(word);
+                    }
+
+                    string summarSentance = string.Join(" ", summary);
+                    Console.WriteLine(summarSentance + "...");
+                }
+                return string.Join(" ", summary);
+            }
+        }
+}
---
+++ TextSummarizingWords/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextSummarizingWords
+{
+    /*User has to enter the Text
+     *User has to enter the length(no of characters) that they want in the summary.
+     *Note - will include only whole word. 
+     *Output - the program will return the first few words in the text according to the length we specify
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the Text, whose summary you want to get : \n");
+            string Sentance = Console.ReadLine();
+            
+            Console.WriteLine("\nPlease specify the length(no of characters) of the summary you require:");
+            int maxLength = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("\nThe Summary(first few words according to the length specified) is : ");
+            CallingSummary.WritingSummary(Sentance, maxLength);//can use class name to directly call because WritingSummary method is static
+        }
+
+    }
+}
+
